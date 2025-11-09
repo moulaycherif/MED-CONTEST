ce79dab7de043b0935f6741aadb309c401dfba16
moulaycherif <moulay2011@gmail.com>
2025-11-09 12:03:12 +0100

    Ajout de la route adminRoutes dans app.ts

--- qcm-frontend/src/pages/StudentPage.tsx
+++ qcm-frontend/src/pages/StudentPage.tsx
@@ -172,7 +172,7 @@ export default function StudentPage() {
                   </div>
                 ))}
               </div>
-            ))}
+            )}
           </div>
         )}
       </main>
