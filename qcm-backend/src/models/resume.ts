cec8ff8beb794a9fd5c5c5fe9c9af4ab1666cac8
moulaycherif <moulay2011@gmail.com>
2025-11-22 20:07:35 +0100

    Mise à jour

--- qcm-backend/src/models/resume.ts
+++ qcm-backend/src/models/resume.ts
@@ -4,7 +4,11 @@ const ResumeSchema = new mongoose.Schema(
   {
     subject: { type: String, required: true },
     chapter: { type: String, required: true },
-    pdfUrl: { type: String, required: true },
+    pdfUrl: {
+  type: String,
+  required: false,  // <-- CHANGER ICI
+  default: null,
+},
     createdAt: { type: Date, default: Date.now }
   },
   { collection: "resumes" },
