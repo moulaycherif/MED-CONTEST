85cc261daca0e5c91bbc169736e737d2af71082e
moulaycherif <moulay2011@gmail.com>
2025-11-23 18:18:03 +0100

    Mise à jour

--- qcm-backend/src/models/resume.ts
+++ qcm-backend/src/models/resume.ts
@@ -4,16 +4,9 @@ const ResumeSchema = new mongoose.Schema(
   {
     subject: { type: String, required: true },
     chapter: { type: String, required: true },
-
-    // PDF encodé en base64
-    pdfBase64: { type: String, required: true },
-
-    // URL pour Ouvrir PDF dans React
-    pdfUrl: { type: String, required: true },
-
-    createdAt: { type: Date, default: Date.now },
+    pdfUrl: { type: String, required: true }, // Fichier sur le serveur
   },
-  { collection: "resumes" }
+  { timestamps: true }
 );
 
 export default mongoose.model("Resume", ResumeSchema);
