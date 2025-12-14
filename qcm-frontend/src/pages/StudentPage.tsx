a8c094a0511431575bdcfb6ee65f8df6a3d8c60d
moulaycherif <moulay2011@gmail.com>
2025-12-14 16:30:54 +0100

    Update all files

--- qcm-frontend/src/pages/StudentPage.tsx
+++ qcm-frontend/src/pages/StudentPage.tsx
@@ -307,7 +307,7 @@ if (section === "soutien" && selectedMatiere) {
     return (
       <div className="p-6">
         <h2 className="text-3xl font-bold text-center mb-6">
-          📘 {selectedChapter} — Résumés disponibles
+          📘 Résumés {selectedMatiere}
         </h2>
 
         <StudentSummaries selectedSubject={selectedMatiere} selectedChapter={selectedChapter} />
