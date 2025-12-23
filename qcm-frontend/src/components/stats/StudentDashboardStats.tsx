1d26085697ad0fb412e4fedb02a624a7d9b5ac1d
moulaycherif <moulay2011@gmail.com>
2025-12-23 18:33:22 +0100

    mise à jour

--- qcm-frontend/src/components/stats/StudentDashboardStats.tsx
+++ qcm-frontend/src/components/stats/StudentDashboardStats.tsx
@@ -1,35 +1,17 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-import { API_BASE_URL } from "../../config";
-import StudentActivityTimeline from "../StudentActivityTimeline";
+import api from "../../api/axios";
 import QcmBarChart from "./QcmBarChart";
-import StudentRanking from "./StudentRanking";
 import ActivityLineChart from "./ActivityLineChart";
-
-
 
 export default function StudentDashboardStats() {
   const [stats, setStats] = useState<any>(null);
-  const [error, setError] = useState(false);
 
   useEffect(() => {
-  const url = `${API_BASE_URL}/api/stats/student/TEMP_STUDENT_ID`;
-  console.log("📡 Appel API stats :", url);
-
-  axios
-    .get(url)
-    .then((res) => {
-      console.log("✅ Réponse stats :", res.data);
+    api.get("/api/stats/student").then((res) => {
       setStats(res.data);
-    })
-    .catch((err) => {
-      console.error("❌ Erreur stats", err);
-      setError(true);
     });
-}, []);
+  }, []);
 
-
-  if (error) return <p>❌ Impossible de charger les statistiques.</p>;
   if (!stats) return <p>Chargement statistiques...</p>;
 
   return (
@@ -37,34 +19,26 @@ export default function StudentDashboardStats() {
       {/* KPIs */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <div className="p-4 bg-white rounded-xl shadow">
-          📘 Concours<br />
-          <b>0 / 0</b>
+          📄 Résumés<br />
+          <b>{stats.resources.find((r:any)=>r._id==="RESUME")?.count || 0}</b>
         </div>
 
         <div className="p-4 bg-white rounded-xl shadow">
-          📄 Résumés consultés<br />
-          <b>
-            {stats.resources?.find((r:any)=>r._id==="RESUME")?.count ?? 0}
-          </b>
+          💡 Astuces<br />
+          <b>{stats.resources.find((r:any)=>r._id==="ASTUCE")?.count || 0}</b>
         </div>
 
         <div className="p-4 bg-white rounded-xl shadow">
-          💡 Astuces consultées<br />
-          <b>
-            {stats.resources?.find((r:any)=>r._id==="ASTUCE")?.count ?? 0}
-          </b>
+          🧩 QCM<br />
+          <b>{stats.resources.find((r:any)=>r._id==="QCM")?.count || 0}</b>
         </div>
       </div>
 
-      {/* Timeline */}
-
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-          <QcmBarChart data={stats.qcmBySubject} />
-          <ActivityLineChart data={stats.timeline} />
-        </div>
-
-      <StudentActivityTimeline />
-      <StudentRanking data={stats.ranking} />
+      {/* Graphes */}
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <QcmBarChart data={stats.qcmBySubject} />
+        <ActivityLineChart data={stats.timeline} />
+      </div>
     </div>
-      );
+  );
 }
