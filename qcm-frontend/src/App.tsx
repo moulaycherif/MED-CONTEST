3bd2c52b790812125780afb2afed0628e7b7af97
moulaycherif <moulay2011@gmail.com>
2026-01-08 11:33:44 +0100

    Fix stats for JWT studentId

--- qcm-frontend/src/App.tsx
+++ qcm-frontend/src/App.tsx
@@ -9,6 +9,9 @@ import StudentPage from "./pages/StudentPage";
 import AdminDashboard from "./pages/AdminDashboard";
 import { API_BASE_URL } from "./config";
 
+console.log("API =", import.meta.env.VITE_API_BASE_URL);
+
+
 export default function App() {
   return (
       <Router>
