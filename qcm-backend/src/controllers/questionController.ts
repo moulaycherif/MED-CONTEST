ed7be9e613c83cb2608ebcabafcaa4414bd7689e
moulaycherif <moulay2011@gmail.com>
2026-01-30 15:02:55 +0100

    Fix Excel import propagation (exam/subject)

--- qcm-backend/src/controllers/questionController.ts
+++ qcm-backend/src/controllers/questionController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import Question from "../models/Question";
 import Exam from "../models/Exam";
 import XLSX from "xlsx";
-import QuestionGroup from "../models/QuestionGroup.ts";
+import QuestionGroup from "../models/QuestionGroup";
 
 console.log("🔥 QUESTION CONTROLLER LOADED");
 console.log("🚨 VERSION QUESTION CONTROLLER 2026-FINAL-GROUP-IMPORT");
