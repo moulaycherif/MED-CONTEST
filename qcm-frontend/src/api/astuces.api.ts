86fc9a43259776fd0c49728b4adb7aeb6e80ea91
moulaycherif <moulay2011@gmail.com>
2026-03-18 13:58:21 +0000

    Fix Excel import propagation (exam/subject)

--- qcm-frontend/src/api/astuces.api.ts
+++ qcm-frontend/src/api/astuces.api.ts
@@ -8,7 +8,7 @@ export interface Astuce {
 export async function fetchAstucesByChapter(chapter: string): Promise<Astuce[]> {
   console.log("API astuces brut :");
   const res = await api.get(`/api/astuces/${encodeURIComponent(chapter)}`);
-  console.log("API astuces brut :", res.data);
+  console.log("API astuces brut AVANT :", res);
   return res.data;
-  console.log("API astuces brut :", res.data);
+  console.log("API astuces brut APRES:", res.data);
 }
