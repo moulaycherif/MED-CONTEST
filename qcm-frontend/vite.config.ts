be871c427127aab01aac610a4243441d26f1e09c
moulaycherif <moulay2011@gmail.com>
2026-03-25 23:26:30 +0100

    fix pdfParse import

--- qcm-frontend/vite.config.ts
+++ qcm-frontend/vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    dedupe: ["react", "react-dom"], // 🔥 TRÈS IMPORTANT
+    dedupe: ["react", "react-dom"],
+    alias: {
+      react: "/node_modules/react",
+      "react-dom": "/node_modules/react-dom",
+    },
   },
 });
