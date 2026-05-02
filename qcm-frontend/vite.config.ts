879df04cafd410254cad1a1beb62b6a1701af37d
moulaycherif <moulay2011@gmail.com>
2026-05-02 17:07:36 +0100

    resolve conflict: keep PdfViewer

--- qcm-frontend/vite.config.ts
+++ qcm-frontend/vite.config.ts
@@ -3,11 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  resolve: {
-    alias: {
-      "pdfjs-dist": "pdfjs-dist/build/pdf"
-    }
-  },
-
 });
