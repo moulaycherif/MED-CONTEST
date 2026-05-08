e0bddd06fbb6064c90918e1e05bfc341aaae0cae
moulaycherif <moulay2011@gmail.com>
2026-05-08 23:13:49 +0100

    fix: add pdf worker

--- qcm-frontend/src/components/RichMathEditor.tsx
+++ qcm-frontend/src/components/RichMathEditor.tsx
@@ -5,6 +5,15 @@ import "react-quill/dist/quill.snow.css";
 import katex from "katex";
 import "katex/dist/katex.min.css";
 
+import Quill from "quill";
+import ImageUploader from "quill-image-uploader";
+import { API_BASE_URL } from "../config";
+
+Quill.register(
+  "modules/imageUploader",
+  ImageUploader
+);
+
 (window as any).katex = katex;
 
 interface Props {
@@ -18,15 +27,45 @@ export default function RichMathEditor({
 }: Props) {
 
   const modules = {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ["bold", "italic", "underline"],
-      [{ list: "ordered" }, { list: "bullet" }],
-      ["link"],
-      ["clean"],
-      ["formula"],
-    ],
-  };
+  toolbar: [
+    [{ header: [1, 2, false] }],
+
+    ["bold", "italic", "underline"],
+
+    [{ list: "ordered" }, { list: "bullet" }],
+
+    ["link", "image"],
+
+    ["formula"],
+
+    ["clean"],
+  ],
+
+  clipboard: {
+    matchVisual: false,
+  },
+
+  imageUploader: {
+    upload: async (file: File) => {
+
+      const formData = new FormData();
+
+      formData.append("image", file);
+
+      const res = await fetch(
+        `${API_BASE_URL}/api/exercises/upload-editor-image`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      const data = await res.json();
+
+      return `${API_BASE_URL}${data.url}`;
+    },
+  },
+};
 
   return (
     <div className="bg-white mb-4">
