39991f3126421b1dcbcb3cb181af515d4757e5af
moulaycherif <moulay2011@gmail.com>
2026-05-25 00:25:33 +0100

    fix: add pdf worker

--- qcm-backend/src/models/QuestionGroup.ts
+++ qcm-backend/src/models/QuestionGroup.ts
@@ -2,6 +2,7 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface IQuestionGroup extends Document {
   image?: string | null;
+  intro?: string | null;
   subject: string;
   exam: string;
   order?: number;
@@ -10,6 +11,7 @@ export interface IQuestionGroup extends Document {
 const questionGroupSchema = new Schema<IQuestionGroup>(
   {
     image: { type: String, default: null },
+    intro: { type: String, default: null },
     subject: { type: String, required: true },
     exam: { type: String, required: true },
     order: { type: Number, default: 0 },
